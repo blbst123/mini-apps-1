ac0bd32d4d4f85a9975f1181b3748788a3e3220a
Bill Lou <billloukehao@gmail.com>
2019-05-06 10:44:01 -0700

    Implement index html format and basic player

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,18 @@
+// PLAYER CREATION
+class Player {
+  constructor() {
+    this.pieces = { 1: false, 2: false, 3: false, 4: false, 5: false, 6: false, 7: false, 8: false, 9: false };
+  }
 
+  makeMove(location) {
+    this.pieces[location] = true;
+  }
+
+  checkWin() {
+    // TODO: check if player has any winning positions
+  }
+}
+
+var PlayerX = new Player;
+var PlayerY = new Player;
 
