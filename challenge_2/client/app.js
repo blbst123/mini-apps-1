68ff1145c1c004f62ebba0ec4f0c11b97c473715
Bill Lou <billloukehao@gmail.com>
2019-05-07 15:54:49 -0700

    Implement working JSON to CSV converter
    - Takes a textarea input as JSON
    - Uses a textarea to display CSV

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,16 @@
+var submitFile = function(e) {
+  e.preventDefault();
 
+  $.ajax({
+    type: "POST",
+    url: "/",
+    contentType: 'application/json',
+    data: JSON.stringify({file: $("#inputField").val()}),
+    success: function(data) {
+      // console.log('success1');
+      $("#outputField").html(data)
+      console.log(data);
+    },
+  });
+}
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,50 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.static('./client'));
 
 
+app.listen(3000);
+
+app.get('/', (req, res) => {
+  console.log('in app.get');
+  res.status(200).send();
+});
+
+app.post('/', (req, res) => {
+  var jsonObj = JSON.parse(req.body.file);
+  var keysArr = Object.keys(jsonObj);
+  let keysStr = keysArr.reduce((accumulator, currVal) => {
+    return accumulator + currVal + ',';
+  }, '');
+
+  keysStr = keysStr.substring(0, keysStr.length - 1) + '\n';
+
+  let valStr = convertToCSV(jsonObj);
+
+  resultStr = keysStr + valStr;
+
+  // console.log(resultStr);
+
+  res.status(201).send(resultStr);
+});
+
+var convertToCSV = function(obj) {
+  let csvStr = '';
+  for (let key in obj) {
+    if (key !== 'children') {
+      csvStr += obj[key] + ',';
+    }
+  }
+  csvStr = csvStr.substring(0, csvStr.length - 1) + '\n';
+
+  if (obj.children !== undefined) {
+    for (let i = 0; i < obj.children.length; i++) {
+      csvStr += convertToCSV(obj.children[i]);
+    }
+  }
+
+  return csvStr;
+}
