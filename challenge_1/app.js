3f91014c1c2b72939518684001bc4986e8ca97cc
Bill Lou <billloukehao@gmail.com>
2019-05-06 17:01:45 -0700

    Implement working spinning mode

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,42 +1,76 @@
 // PLAYER CREATION
 class Player {
-  constructor(symbol) {
+  constructor(symbol, name) {
     this.symbol = symbol;
+    this.name = name;
+    this.score = 0;
+
+    document.getElementById("playerName" + symbol).innerHTML = `Player ${name} (Symbol ${symbol}): `;
+  }
+
+  updateScore() {
+    this.score++;
   }
 }
-
-var PlayerX = new Player('X');
-var PlayerO = new Player('O');
 
 // BOARD INITIALIZATION
 class Board {
   constructor() {
-    this.currentPlayer = PlayerX;
 
-    this.pieces = [['0', '1', '2'],['3', '4', '5'],['6', '7', '8']];
+    let name1 = prompt("Please enter your name Player 1:", "X");
+    let name2 = prompt("Please enter your name Player 2:", "O");
+
+    this.PlayerX = new Player('X', name1);
+    this.PlayerO = new Player('O', name2);
+
+    this.currentPlayer = this.PlayerX;
+    this.startingPlayer = this.PlayerX;
+
+    this.pieces = [['', '', ''], ['', '', ''], ['', '', '']];
     this.gameRunning = true;
     this.piecesPlaced = 0;
+    this.rotating = false;
+    this.currentRotation = 1;
+    this.pause = false;
   }
 
-  makeMove(location, symbol) {
+  makeMove(event) {
+    console.log(this.currentRotation);
+    if (this.rotating || this.pause) return;
+    this.pause = true;
+
+    let location = event.id;
+    let symbol = this.currentPlayer.symbol;
+
     let row = Math.floor(location / 3);
     let col = location % 3;
 
+    if (this.pieces[row][col] !== '') {
+      return;
+    }
+    event.innerHTML = this.currentPlayer.symbol;
     this.pieces[row][col] = symbol;
     this.piecesPlaced++;
 
+    // If game is won on move
     if (this.checkWin(symbol)) {
       this.gameRunning = false;
       this.piecesPlaced = 0;
-      document.getElementById("winner").innerHTML = "Game Won!! Congrats Player" + this.currentPlayer.symbol;
+      this.currentPlayer.updateScore();
+      this.startingPlayer = this.currentPlayer;
+      document.getElementById("score" + this.currentPlayer.symbol).innerHTML = this.currentPlayer.score;
+      document.getElementById("winner").innerHTML = "Game Won!! Congrats Player " + this.currentPlayer.symbol;
     }
     else {
+      // If game is drawn on move
       if (this.piecesPlaced === 9) {
         this.gameRunning = false;
         this.piecesPlaced = 0;
         document.getElementById("winner").innerHTML = 'Draw';
       }
       else {
+        this.pause = false;
+        this.rotateBoard();
         this.swapPlayers();
       }
     }
@@ -44,11 +78,11 @@ class Board {
 
   swapPlayers() {
     if (this.currentPlayer.symbol === 'X') {
-      this.currentPlayer = PlayerO;
+      this.currentPlayer = this.PlayerO;
       document.getElementById("turn").innerHTML = this.currentPlayer.symbol;
     }
     else {
-      this.currentPlayer = PlayerX;
+      this.currentPlayer = this.PlayerX;
       document.getElementById("turn").innerHTML = this.currentPlayer.symbol;
     }
   }
@@ -78,32 +112,107 @@ class Board {
   }
 
   resetGame() {
-    this.pieces = [['1', '2', '3'],['4', '5', '6'],['7', '8', '9']];
-    this.currentPlayer = PlayerX;
+    this.pieces = [['', '', ''], ['', '', ''], ['', '', '']];
+    this.currentPlayer = this.startingPlayer;
     this.gameRunning = true;
-    var locations = document.getElementsByClassName("location");
-    for (let i = 0; i < locations.length; i++) {
-      locations[i].innerHTML = '';
-    }
+    this.pause = false;
+    this.currentRotation = 1;
+    this.renderScreen();
 
-    document.getElementById("winner").innerHTML = 'Game still running';
+    // Remove all classes from board
+    var classes = document.getElementById("board").classList;
+    for (let i = 0; i < classes.length; i++) {
+      document.getElementById("board").classList.remove(classes[i]);
+    }
+    document.getElementById("winner").innerHTML = 'Game Running';
+  }
+
+  rotateBoard() {
+    if (this.rotating) return;
+    document.getElementById("board").className = "rotateTable" + this.currentRotation;
+    this.rotating = true;
+    setTimeout(() => {
+      this.gravityDrop();
+      setTimeout(() => {
+        this.renderScreen();
+        this.rotating = false;
+      }, 300);
+    }, 1500);
+  }
+
+  // GRAVITY DROP FUNCTIONS
+  gravityDrop() {
+    // Drop to the right
+    if (this.currentRotation === 1) {
+      for (let row = 0; row < 3; row++) {
+        this.dropRight(row, 1);
+        this.dropRight(row, 0);
+      }
+      this.currentRotation++;
+    }
+    else if (this.currentRotation === 2) {
+      for (let col = 0; col < 3; col++) {
+        this.dropUp(1, col);
+        this.dropUp(2, col);
+      }
+      this.currentRotation++;
+    }
+    else if (this.currentRotation === 3) {
+      for (let row = 0; row < 3; row++) {
+        this.dropLeft(row, 1);
+        this.dropLeft(row, 2);
+      }
+      this.currentRotation++;
+    }
+    else {
+      for (let col = 0; col < 3; col++) {
+        this.dropDown(1, col);
+        this.dropDown(0, col);
+      }
+      this.currentRotation = 1;
+    }
+  }
+
+  dropRight(row, col) {
+    if (col <= 1 && this.pieces[row][col + 1] === '') {
+      this.pieces[row][col + 1] = this.pieces[row][col];
+      this.pieces[row][col] = '';
+      this.dropRight(row, col + 1);
+    }
+  }
+
+  dropUp(row, col) {
+    if (row >= 1 && this.pieces[row - 1][col] === '') {
+      this.pieces[row - 1][col] = this.pieces[row][col];
+      this.pieces[row][col] = '';
+      this.dropUp(row - 1, col);
+    }
+  }
+
+  dropLeft(row, col) {
+    if (col >= 1 && this.pieces[row][col - 1] === '') {
+      this.pieces[row][col - 1] = this.pieces[row][col];
+      this.pieces[row][col] = '';
+      this.dropLeft(row, col - 1);
+    }
+  }
+
+  dropDown(row, col) {
+    if (row <= 1 && this.pieces[row + 1][col] === '') {
+      this.pieces[row + 1][col] = this.pieces[row][col];
+      this.pieces[row][col] = '';
+      this.dropDown(row + 1, col);
+    }
+  }
+
+  renderScreen() {
+    for (let row = 0; row < this.pieces.length; row++) {
+      for (let col = 0; col < this.pieces[row].length; col++) {
+        let id = row * 3 + col;
+        document.getElementById(`${id}`).innerHTML = this.pieces[row][col];
+      }
+    }
   }
 }
 
 var board = new Board();
-
-var makeMove = function (event) {
-  if (board.gameRunning) {
-    event.innerHTML = board.currentPlayer.symbol;
-    board.makeMove(event.id, board.currentPlayer.symbol);
-  }
-  else {
-    // alert("Please reset game to try again.");
-  }
-}
-
-var resetGame = function () {
-  board.resetGame();
-}
-
-// document.getElementByClass("location").onclick = function() {myFunction()};
