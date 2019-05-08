7a39dc3b0fb9b043f6fa1a55a918429ffa4b1498
Bill Lou <billloukehao@gmail.com>
2019-05-08 13:31:18 -0700

    Implement bare working react app

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,8 @@
+var express = require('express');
+var app = express();
+const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
